812b573c4c0bd7a1c563f4bf72ab40a2cdee7c2c
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2025-02-11 16:02:47 +0200

    feat: integrate solitier game via iframe

---
+++ src/app/solitier-game/page.js
@@ -0,0 +1,12 @@
+import SolitierGame from '@/components/SolitierGame';
+
+export const metadata = {
+  title: 'Solitier Game',
+  description: 'Solitier Game',
+};
+
+const SolitierPage = () => {
+  return <SolitierGame />;
+};
+
+export default SolitierPage;
